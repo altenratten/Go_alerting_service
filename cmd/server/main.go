18f1f55806ce06f6a7d677afc8fee4dc07c4bfca
altenratten <93007177+altenratten@users.noreply.github.com>
2024-10-02 08:54:17 +0300

    modified:   cmd/server/main.go 	new file:   go.mod

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,47 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func mainPage (res http.ResponseWriter, req *http.Request) {
+	body := fmt.Sprintf("Method: %s\r\n", req.Method)
+    body += "Header ===============\r\n"
+    for k, v := range req.Header {
+        body += fmt.Sprintf("%s: %v\r\n", k, v)
+    }
+    body += "Query parameters ===============\r\n"
+    for k, v := range req.URL.Query() {
+        body += fmt.Sprintf("%s: %v\r\n", k, v)
+    }
+    res.Write([]byte(body))
+}
+
+func apiGauge(res http.ResponseWriter, req *http.Request) {
+    res.Write([]byte("You in apiGauge!"))
+}
+
+func apiCounter (res http.ResponseWriter, req *http.Request) {
+    res.Write([]byte("You in apiCounter!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir(".."))
+
+	mux.HandleFunc("/", mainPage)
+	mux.HandleFunc("/update/gauge/", apiGauge)
+	mux.HandleFunc("/update/counter/", apiCounter)
+
+    //mux.Handle(`POST /update/{type}/{name}/{value}`, handlers.UpdateHandler(storage))
+
+
+
+
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/altenratten/Go_alerting_service
+
+go 1.23.1
