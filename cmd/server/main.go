94a098b75c17ce9d03b6efd1d540d8f29a442f93
altenratten <93007177+altenratten@users.noreply.github.com>
2024-10-03 23:13:24 +0300

    modified:   cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
+
+var gstorage = make(map[string]float64)
+var cstorage = make(map[string]int64)
 
 func mainPage (res http.ResponseWriter, req *http.Request) {
 	body := fmt.Sprintf("Method: %s\r\n", req.Method)
@@ -12,33 +17,57 @@ func mainPage (res http.ResponseWriter, req *http.Request) {
         body += fmt.Sprintf("%s: %v\r\n", k, v)
     }
     body += "Query parameters ===============\r\n"
+	body += `<a href="http://127.0.0.1:8080/update/gggg/rrr/21">link text</a>`
     for k, v := range req.URL.Query() {
         body += fmt.Sprintf("%s: %v\r\n", k, v)
     }
     res.Write([]byte(body))
 }
 
-func apiGauge(res http.ResponseWriter, req *http.Request) {
-    res.Write([]byte("You in apiGauge!"))
-}
 
-func apiCounter (res http.ResponseWriter, req *http.Request) {
-    res.Write([]byte("You in apiCounter!"))
+func handler(w http.ResponseWriter, r *http.Request) {
+	// get the value for the greeting wildcard.
+	t := r.PathValue("type")
+	n := r.PathValue("name")
+	v := r.PathValue("value")
+
+	switch t {
+	case `gauge`:
+
+		s, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			w.Write([]byte(`http.StatusBadRequest`))
+		}
+		gstorage[n] = s
+
+		jsonString, _ := json.Marshal(gstorage)
+		
+		w.Write([]byte(jsonString))
+
+	case `counter`:
+		s, err := strconv.ParseInt(v, 10, 64)
+		if err != nil {
+			w.Write([]byte(`http.StatusBadRequest`))
+		}
+
+		cstorage[n] = s
+
+		jsonString, _ := json.Marshal(gstorage)
+		
+		w.Write([]byte(jsonString))
+
+	default:
+		w.Write([]byte(`http.StatusNotFound`))
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir(".."))
 
 	mux.HandleFunc("/", mainPage)
-	mux.HandleFunc("/update/gauge/", apiGauge)
-	mux.HandleFunc("/update/counter/", apiCounter)
+	mux.HandleFunc("/update/{type}/{name}/{value}", handler)
 
     //mux.Handle(`POST /update/{type}/{name}/{value}`, handlers.UpdateHandler(storage))
-
-
-
-
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
