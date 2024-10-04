9035ad01ea40c30be3315230fba96c77ed432d25
altenratten <93007177+altenratten@users.noreply.github.com>
2024-10-04 06:52:31 +0300

    modified:   cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,22 +10,23 @@ import (
 var gstorage = make(map[string]float64)
 var cstorage = make(map[string]int64)
 
-func mainPage (res http.ResponseWriter, req *http.Request) {
-	body := fmt.Sprintf("Method: %s\r\n", req.Method)
-    body += "Header ===============\r\n"
-    for k, v := range req.Header {
-        body += fmt.Sprintf("%s: %v\r\n", k, v)
-    }
-    body += "Query parameters ===============\r\n"
-	body += `<a href="http://127.0.0.1:8080/update/gggg/rrr/21">link text</a>`
-    for k, v := range req.URL.Query() {
-        body += fmt.Sprintf("%s: %v\r\n", k, v)
-    }
-    res.Write([]byte(body))
+func mainPage (w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`http.StatusNotFound`))
+	// body := fmt.Sprintf("Method: %s\r\n", req.Method)
+    // body += "Header ===============\r\n"
+    // for k, v := range req.Header {
+    //     body += fmt.Sprintf("%s: %v\r\n", k, v)
+    // }
+    // body += "Query parameters ===============\r\n"
+	// body += `<a href="http://127.0.0.1:8080/update/gggg/rrr/21">link text</a>`
+    // for k, v := range req.URL.Query() {
+    //     body += fmt.Sprintf("%s: %v\r\n", k, v)
+    // }
+    // res.Write([]byte(body))
 }
 
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func Handlers(w http.ResponseWriter, r *http.Request) {
 	// get the value for the greeting wildcard.
 	t := r.PathValue("type")
 	n := r.PathValue("name")
@@ -52,9 +53,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 		cstorage[n] = s
 
-		jsonString, _ := json.Marshal(gstorage)
+		// jsonString, _ := json.Marshal(gstorage)
 		
-		w.Write([]byte(jsonString))
+		// w.Write([]byte(jsonString))
 
 	default:
 		w.Write([]byte(`http.StatusNotFound`))
@@ -65,9 +66,9 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", mainPage)
-	mux.HandleFunc("/update/{type}/{name}/{value}", handler)
+	mux.HandleFunc("/update/{type}/{name}/{value}", Handlers)
 
-    //mux.Handle(`POST /update/{type}/{name}/{value}`, handlers.UpdateHandler(storage))
+    //mux.Handle(`POST /update/{type}/{name}/{value}`, Handlers.UpdateHandlers(storage))
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
